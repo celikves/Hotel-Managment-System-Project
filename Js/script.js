66e127073678e148be3b20b5f7a4038bfd6d21a4
Vesile <vesile_emre@hoymail.com>
2021-05-02 17:55:33 +0300

    Fixing some problems on pages

--- Js/script.js
+++ Js/script.js
@@ -170,5 +170,19 @@ var options = {
 var chart = new ApexCharts(document.querySelector("#apex3"), options);
 chart.render();
 
+/* For homepage slider */
+var myIndex = 0;
+                carousel();
 
+                function carousel() {
+                    var i;
+                    var x = document.getElementsByClassName("mySlides");
+                    for (i = 0; i < x.length; i++) {
+                        x[i].style.display = "none";
+                    }
+                    myIndex++;
+                    if (myIndex > x.length) { myIndex = 1 }
+                    x[myIndex - 1].style.display = "block";
+                    setTimeout(carousel, 4000); // Change image every 4 seconds
+                }
 
